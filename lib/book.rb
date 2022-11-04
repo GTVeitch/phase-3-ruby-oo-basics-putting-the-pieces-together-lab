be43ab58dc31f67c2d46ffefb0efa6911ed55002
Gavin Veitch <gavintveitch@gmail.com>
2022-11-04 11:23:24 -0600

    commit tree 02da97236c61856348f478583479bca323abdc12

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,10 @@
-
-
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize (author= "Agatha Christie", title = "And Then There Were None")
+        @author = author
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
